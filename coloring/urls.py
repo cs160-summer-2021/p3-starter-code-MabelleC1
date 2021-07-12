a91bb8ee555cd502906c6c5263ec95937458eb54
Mabelle Cornelius <mabelinegcornelius@gmail.com>
2021-07-11 22:27:56 -0700

    Home screen pretty much done

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
+    path('screen1', views.screen1, name='screen1'),
+    path('createnew', views.createnew, name='createnew'),
+    path('templates', views.templates, name='templates'),
+    path('open', views.createnew, name='open'),
     path('new_interaction', views.index, name='new_interaction')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def screen1(request):
+    return render(request, 'coloring/screen1.html')
+
+def createnew(request):
+    return render(request, 'coloring/createnew.html')
+
+def templates(request):
+    return render(request, 'coloring/templates.html')
+
+def open(request):
+    return render(request, 'coloring/open.html')
